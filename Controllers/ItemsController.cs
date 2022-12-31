84896487fe56efd6cae8dfb402d9b527710e3b43
Alvin <Alvin@TORTOISEGENIE-1>
2022-12-31 14:40:45 -0500

    Add project files.

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Service.Dtos;
+
+namespace Play.Catalog.Service.Controllers
+{
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> items = new List<ItemDto>()
+            {
+                new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow),
+                new ItemDto(Guid.NewGuid(), "Antidote", "Cures poiso", 7, DateTimeOffset.UtcNow),
+                new ItemDto(Guid.NewGuid(), "Bronze sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow)
+            };
+
+        [HttpGet]
+        [Route("items")]
+        public IEnumerable<ItemDto> Get()
+        {
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetByID(Guid id)
+        {
+            var item = items.Where(item => item.id == id).FirstOrDefault(); 
+
+            if (item== null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        [HttpPost]
+        [Route("items")]
+        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+        {
+            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+
+            items.Add(item);
+
+            return CreatedAtAction(nameof(GetByID), new { id = item.id }, item);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            var existingItem = items.Where(item => item.id == id).SingleOrDefault();
+
+            var updateItem = existingItem with
+            {
+                Name = updateItemDto.Name,
+                Description = updateItemDto.Description,
+                Price = updateItemDto.Price
+            };
+
+            var index = items.FindIndex(existingItem => existingItem.id == id);
+
+            items[index] = updateItem;
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ Entities/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace Play.Catalog.Service.Entities
+{
+    public class Item
+    {
+        public Guid id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; } 
+
+        public int Price { get; set; }  
+
+        public DateTimeOffset CreatedDate { get; set; }
+
+    }
+}
